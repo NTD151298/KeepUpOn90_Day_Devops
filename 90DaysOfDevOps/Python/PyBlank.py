e83873dc341ce753f6c9764274879436a2933561
NTD1512 <duongtn1512@gmail.com>
2023-06-16 09:47:04 +0700

    yes

---
+++ 90DaysOfDevOps/Python/PyBlank.py
@@ -0,0 +1,18 @@
+# Make a is an interger, and have value of 10
+a = int(10)
+print(a)
+
+# Make function that have value of m
+
+
+def fun_1(n):
+    # m is a string and will have value of string n and Hello world
+    m = str(str(n) + " Hello world")
+    # return m
+    return m
+
+
+# value a go though function fun_1
+fun_1(a)
+# print the value of function fun_1(a)
+print(fun_1(a))
